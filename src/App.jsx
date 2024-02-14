8f9f4da48caecf2a8f5129143e66c92d598c9a6f
Agustin Ancona <anconaa54@gmail.com>
2024-02-14 16:23:02 -0300

    Agregando cambios

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Header from './components/Header/Header'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer';
 
-import('./app.css')
-
 export default function App() {
   return (
     <>
