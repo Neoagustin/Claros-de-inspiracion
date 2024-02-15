9f4c9543cd556b040fe61d6cfeebeef8cf8c7f90
Agustin Ancona <anconaa54@gmail.com>
2024-02-15 16:10:13 -0300

    Agregando cambios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Home.css';
 import './HomeResponsive.css';
 import Banner from '../../components/Banner/Banner';
-import FireSound from '../../components/FireSound/FireSound';
 import HistorySection from '../../components/HomeSections/HomeSections';
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
     <>
       <main className='main-home'>
         <Banner />
-        <FireSound />
         <HistorySection />
       </main>
     </>
