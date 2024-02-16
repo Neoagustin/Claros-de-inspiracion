e2efa9f391e60cc3b4f4fa130a1d657efdb6734e
Agustin Ancona <anconaa54@gmail.com>
2024-02-16 15:19:13 -0300

    Agregando cambios

--- src/pages/Offerings/Offerings.jsx
+++ src/pages/Offerings/Offerings.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import './Offerings.css'
+import offeringImage from '../../assets/img/ofrendas.jpg';
+import './Offerings.css';
 
 export default function Offerings() {
    return (
-      <div className='offering-container'>
-         <h1>Ofrendas</h1>
-         <p>Buenas! Dios te bendiga, queremos contarte que podes ser parte de este ministerio y del avance del Reino de los Cielos por medio de tus ofrendas.</p>
-         <p>Las ofrendas están destinadas a sostener el ministerio financieramente. Tanto para el crecimiento y desarrollo de la página web y de los cursos, cómo también los viajes misioneros que se realicen, esto es para bendecir a las Iglesias en distintas partes del mundo.</p>
-         <p>Podes ofrendar por Mercado Pago si sos de Argentina, acá te dejamos el link:</p>
-         <a href="https://link.mercadopago.com.ar/ministerioprofetico" target="_blank" rel="noopener noreferrer">link.mercadopago.com.ar/ministerioprofetico</a>
-         <p>(Elegís el monto y listo)</p>
-         <p>En caso de no tener Mercado Pago, podes ofrendar por transferencia al CBU:</p>
-         <p>0000007900203802802494</p>
-         <p>Si sos del exterior podés enviar por PayPal, a este link:</p>
-         <a href="https://www.paypal.me/Despertaraloeterno" target="_blank" rel="noopener noreferrer">paypal.me/Despertaraloeterno</a>
+      <div className='offerings-main-container'>
+         <div className='offerings-content'>
+            <div className="offerings-text">
+               <h1>Ofrendas</h1>
+               <p>Buenas! Dios te bendiga, queremos contarte que puedes ser parte de este ministerio y del avance del Reino de los Cielos mediante tus ofrendas.</p>
+               <p>Las ofrendas están destinadas a sostener el ministerio financieramente, tanto para el crecimiento y desarrollo de la página web y de los cursos, como también para los viajes misioneros que se realicen, todo ello para bendecir a las Iglesias en distintas partes del mundo.</p>
+               <div className="image-container">
+                  <img src={offeringImage} alt="Ofrendas" className="offerings-image" />
+               </div>
+               <p>Puedes ofrendar a través de Mercado Pago si eres de Argentina. Aquí tienes el enlace:</p>
+               <a href="https://link.mercadopago.com.ar/ministerioprofetico" target="_blank" rel="noopener noreferrer">link.mercadopago.com.ar/ministerioprofetico</a>
+               <p>(Seleccionas el monto y listo)</p>
+               <p>En caso de no tener Mercado Pago, puedes ofrendar mediante transferencia al CBU:</p>
+               <p id='CBU'>0000007900203802802494</p>
+               <p>Si estás en el extranjero, puedes enviar tu ofrenda a través de PayPal, en este enlace:</p>
+               <a href="https://www.paypal.me/Despertaraloeterno" target="_blank" rel="noopener noreferrer">paypal.me/Despertaraloeterno</a>
+            </div>
+         </div>
       </div>
    );
 }
