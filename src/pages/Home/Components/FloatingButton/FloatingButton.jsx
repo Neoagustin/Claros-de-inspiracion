f170008bb68168a1f7d23127375db57a19baa99a
Agustin Ancona <anconaa54@gmail.com>
2024-03-07 18:42:47 -0300

    Agregando cambios

--- src/pages/Home/Components/FloatingButton/FloatingButton.jsx
+++ src/pages/Home/Components/FloatingButton/FloatingButton.jsx
@@ -1,54 +1,27 @@
-import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './FloatingButton.css';
 
 function FloatingButton() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [isDragging, setIsDragging] = useState(false);
+  const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
-    const initialX = window.innerWidth - 80;
-    const initialY = window.innerHeight - 300;
-    setPosition({ x: initialX, y: initialY });
+    function handleScroll() {
+      const scrollPosition = window.scrollY;
+      // Puedes ajustar esta posición según tu diseño y preferencias
+      const triggerPosition = 250; // por ejemplo, se activará cuando el scroll haya avanzado 200px
+      setShowButton(scrollPosition > triggerPosition);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
-  const handleTouchStart = (e) => {
-    setIsDragging(true);
-    setPosition({
-      x: e.touches[0].clientX - e.target.offsetLeft,
-      y: e.touches[0].clientY - e.target.offsetTop
-    });
-  };
-
-  const handleTouchMove = (e) => {
-    if (isDragging) {
-      const newX = e.touches[0].clientX - e.target.offsetLeft;
-      const newY = e.touches[0].clientY - e.target.offsetTop;
-
-      // Verificar los límites de la ventana del navegador
-      const maxX = window.innerWidth - 70;
-      const maxY = window.innerHeight - 70;
-
-      // Ajustar la posición si excede los límites
-      const adjustedX = Math.max(0, Math.min(newX, maxX));
-      const adjustedY = Math.max(0, Math.min(newY, maxY));
-
-      setPosition({ x: adjustedX, y: adjustedY });
-    }
-  };
-
-  const handleTouchEnd = () => {
-    setIsDragging(false);
-  };
-
   return (
-    <div
-      className={`floating-button ${isDragging ? 'dragging' : ''}`}
-      style={{ top: position.y, left: position.x }}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
+    <div className={`floating-button ${showButton ? 'active' : ''}`}>
       <Link to="/Cursos">Cursos</Link>
     </div>
   );
